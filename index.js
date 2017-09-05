75bf4ae5cde4667a5bdd8946fb210e60854e8c3d
Patrick Force <patrickforce@gmail.com>
2017-09-04 22:23:36 -0600

    example changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('This is a test cloud garden project.  Here is the value of our test environment variable: ' + process.env.TEST_PROJECT_ENV_VALUE);
+  res.send('The test cloud garden project is running!');
 })
 
 app.listen(3000, function () {
