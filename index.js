3551b0365c45901e1bdefb1e1fef2df8e8186af8
Patrick Force <patrickforce@gmail.com>
2017-09-09 07:17:24 -0600

    update for feature/one

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('The test cloud garden project is running!');
+  res.send('The test cloud garden project (feature/one) is running!');
 })
 
 app.listen(3000, function () {
